843593d1fdab30e78acf3ec54001739768c09f7c
rrobert-lab <ravinrobert@gmail.com>
2022-12-28 02:14:41 -0500

    cs

--- src/App.js
+++ src/App.js
@@ -5,28 +5,72 @@ import Card from "./card"
 
 export default function App() {
   const[cardDeck, setCardDeck]=React.useState(NewCard())
+  let matchcard=[]
+  let idrecord=[]
+
 
 //generate cards
 function NewCard() {
-  const newDice = []
+  var oldNum = [1,2,3,4,5,6,7,8,9,10,1,2,3,4,5,6,7,8,9,10]
+  var newNum=[]
+
   for (let i = 0; i < 20; i++) {
-      newDice.push(generateNewCards())
+    const random = oldNum[Math.floor(Math.random() * oldNum.length)];
+    removeFirst(oldNum, random)
+    
+      newNum.push(generateNewCards(random))
   }
-  return newDice
+  return newNum
 }
 
-
-function holdCard(id) {
-  setCardDeck(oldCard => oldCard.map(Cards => {
-      return Cards.id === id ? 
-          {...Cards, isHeld: !Cards.isHeld} :
-          Cards
-  }))
+function removeFirst(arr, target) {
+  var idx = arr.indexOf(target);
+  if (idx > -1) {
+    arr.splice(idx, 1);
+  }
+  return arr;
 }
 
-function generateNewCards() {
+function holdCard(id, value) {
+
+console.log(idrecord)
+
+
+if(idrecord[0]!==id){
+idrecord.push(id)
+matchcard.push(value)
+matchcard[0]===matchcard[1]?
+
+setCardDeck(oldCard => oldCard.map(Cards => {
+    
+  return Cards.id ===id? 
+  {...Cards, match: true}:
+      Cards
+}))
+
+
+
+:console.log("not match")
+}
+matchcard.length>2?matchcard=idrecord=[]
+:console.log(cardDeck);
+
+ 
+  // setCardDeck(oldCard => oldCard.map(Cards => {
+    
+  //     return Cards.id === id ? 
+  //         {...Cards, isHeld: !Cards.isHeld} :
+  //         Cards
+  // }))
+
+  
+  
+}
+
+function generateNewCards(value) {
           return {
-          value: Math.ceil(Math.random() * 6),
+          
+          value: value,
           id: nanoid(),
           match:false
         }
@@ -40,7 +84,7 @@ const CardElements =  cardDeck.map(card => (
   key={card.id} 
   value={card.value} 
   match={card.match}
-  holdDice={() => holdCard(card.id)}
+  holdCard={() => holdCard(card.id, card.value)}
   />))
 
 
--- src/card.js
+++ src/card.js
@@ -2,9 +2,17 @@ import React from "react"
 
 export default function Card(props) {
   return (
-      <div className="card-face">
-          <h2 className="card-num">{props.value}</h2>
-          
+      <div className=" card-face "
+      onClick={
+        
+        ()=>props.holdCard(props.id, props.value)
+       }
+      
+       > 
+     
+          <h2 className="card-num " >{props.value}</h2>
+         
       </div>
   )
+  
 }
